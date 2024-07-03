6a8312622122108196bca7d6ebbaa47b4a4d0f65
ROPERO17 <146777519+ROPERO17@users.noreply.github.com>
2024-07-03 12:14:50 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,48 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  function paloAleatorio() {
+    let paloIndex = Math.floor(Math.random() * palos.length);
+    return palos[paloIndex];
+  }
+
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function numeroAleatorio() {
+    let numberIndex = Math.floor(Math.random() * numeros.length);
+    return numeros[numberIndex];
+  }
+
+  let paloSeleccionado = paloAleatorio();
+  let numeroSeleccionado = numeroAleatorio();
+
+  document.getElementById("palo-superior").innerHTML = paloSeleccionado;
+  document.getElementById("palo-inferior").innerHTML = paloSeleccionado;
+  document.getElementById("numero-final").innerHTML = numeroSeleccionado;
+
+  if (paloSeleccionado === "♥" || paloSeleccionado === "♦") {
+    document.getElementById("palo-superior").style.color = "red";
+    document.getElementById("palo-inferior").style.color = "red";
+    document.getElementById("numero-final").style.color = "red";
+  } else {
+    document.getElementById("palo-superior").style.color = "black";
+    document.getElementById("palo-inferior").style.color = "black";
+    document.getElementById("numero-final").style.color = "black";
+  }
 };
